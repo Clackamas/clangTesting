2a39747e5d551cbae86fc4a1f0d2e696481ae220
Henry M Pierce <20483214+Clackamas@users.noreply.github.com>
2021-11-25 19:08:02 -0800

    Update

--- src/clangtest1.cpp
+++ src/clangtest1.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+
 int main() {
+    int aBogusValue1 = 1;
+    int aBogusValue2 = 2;
+
+    aBogusValue1 += aBogusValue2++;
+
     std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
